4eb6fde13b927a25d416b67e317794768e19162b
Ebin21 <ebin.alex@outlook.com>
2026-05-02 13:54:06 +0530

    Push all commit ml

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 DATA_PATH = "data/raw/House_Price.csv"
+#DATA_PATH = "data/raw/Housing.csv"
+
 MODEL_PATH = "models/linear_regression.pkl"
 
 df = load_data(DATA_PATH)
@@ -38,38 +40,92 @@ print(interpret_results(rmse, r2))
 # ==============================
 # 1. Actual vs Predicted Plot
 # ==============================
-plt.figure()
+
+plt.figure(figsize=(8,6))
+plt.style.use('ggplot')
 plt.scatter(y_test, y_pred)
+
+# Perfect prediction line (IMPORTANT)
+plt.plot([y_test.min(), y_test.max()],
+         [y_test.min(), y_test.max()],
+         color='red')
+
 plt.xlabel("Actual Prices")
 plt.ylabel("Predicted Prices")
 plt.title("Actual vs Predicted House Prices")
+
 plt.savefig("actual_vs_predicted.png")
 plt.show()
-
 # ==============================
 # 2. Residual Plot
 # ==============================
+
 residuals = y_test - y_pred
 
-plt.figure()
+plt.figure(figsize=(8,6))
+plt.style.use('ggplot')
 plt.scatter(y_pred, residuals)
+
+plt.axhline(y=0, color='red')  # zero error line
+
 plt.xlabel("Predicted Prices")
 plt.ylabel("Residuals")
 plt.title("Residual Plot")
-plt.axhline(y=0)
+
 plt.savefig("residual_plot.png")
 plt.show()
-
 # ==============================
 # 3. Error Distribution
 # ==============================
-plt.figure()
+plt.figure(figsize=(8,6))
+plt.style.use('ggplot')
 plt.hist(residuals, bins=30)
+
+plt.axvline(x=0, color='red')  # zero error center
+
 plt.xlabel("Error")
 plt.ylabel("Frequency")
 plt.title("Error Distribution")
+
 plt.savefig("error_distribution.png")
 plt.show()
+
+# # ==============================
+# # 1. Actual vs Predicted Plot
+# # ==============================
+# plt.figure()
+# plt.scatter(y_test, y_pred)
+# plt.xlabel("Actual Prices")
+# plt.ylabel("Predicted Prices")
+# plt.title("Actual vs Predicted House Prices")
+# plt.savefig("actual_vs_predicted.png")
+# plt.show()
+
+# # ==============================
+# # 2. Residual Plot
+# # ==============================
+# residuals = y_test - y_pred
+
+# plt.figure()
+# plt.scatter(y_pred, residuals)
+# plt.xlabel("Predicted Prices")
+# plt.ylabel("Residuals")
+# plt.title("Residual Plot")
+# plt.axhline(y=0)
+# plt.savefig("residual_plot.png")
+# plt.show()
+
+# # ==============================
+# # 3. Error Distribution
+# # ==============================
+# plt.figure()
+# plt.hist(residuals, bins=30)
+# plt.xlabel("Error")
+# plt.ylabel("Frequency")
+# plt.title("Error Distribution")
+# plt.savefig("error_distribution.png")
+# plt.show()
+# # ==============================================================
 
 # print("RMSE:", rmse)
 # print("R2 Score:", r2)
